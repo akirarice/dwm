fcaa941142929c70f3c77737abb1640ed6067a00
akn <akira.nakano.cbe@gmail.com>
2021-08-01 13:54:58 +0200

    remove tiling symbol and replace with plain text

--- config.def.h
+++ config.def.h
@@ -40,7 +40,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "",      tile },    /* first entry is default */
+	{ "[T]",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
